30db1daf3ceba77f07ba3d92ac398b47918f5370
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2022-06-28 10:33:37 -0700

    Update hypres.small

--- squids/hypres/small.py
+++ squids/hypres/small.py
@@ -14,13 +14,15 @@ def make_polygons():
             coords[name] = array
     films = ["fc", "fc_shield", "pl", "pl_shield"]
     holes = ["fc_center", "pl_center"]
+    abstract_regions = ["bounding_box"]
     sc_polygons = {name: sc.Polygon(name, points=coords[name]) for name in films}
     sc_holes = {name: sc.Polygon(name, points=coords[name]) for name in holes}
-    return sc_polygons, sc_holes
+    sc_abstract = {name: sc.Polygon(name, points=coords[name]) for name in abstract_regions}
+    return sc_polygons, sc_holes, sc_abstract
 
 
 def make_squid(align_layers: str = "middle"):
-    polygons, holes = make_polygons()
+    polygons, holes, abstract_regions = make_polygons()
     layers = hypres_squid_layers(align=align_layers)
     layer_mapping = {
         "fc": "BE",
@@ -36,14 +38,7 @@ def make_squid(align_layers: str = "middle"):
     for name, poly in holes.items():
         poly.layer = layer_mapping[name]
         poly.points = poly.resample(201)
-    bbox = np.array(
-        [
-            [-4.5, -9.5],
-            [8.5, -9.5],
-            [8.5, 3.5],
-            [-4.5, 3.5],
-        ]
-    )
+    bbox = abstract_regions["bounding_box"]
     bounding_box = sc.Polygon("bounding_box", layer="BE", points=bbox)
     return sc.Device(
         "hypres_400nm",
